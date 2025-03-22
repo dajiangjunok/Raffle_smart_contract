249c7c7d7f62745ace828e14db7acc6049955fcc
Alivin <wangyongjie@mediinfo.com.cn>
2025-03-22 14:18:33 +0800

    feat: add chainlink Automation

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -10,14 +10,14 @@
 // Functions
 
 // Layout of Functions:
-// constructor
+// constructor 构造器
 // receive function (if exists)
 // fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
+// external  外部调用函数
+// public  公共函数
+// internal  内部调用函数
+// private  私有函数
+// view & pure functions  无副作用函数
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
@@ -33,6 +33,19 @@ import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2
  */
 contract Raffle is VRFConsumerBaseV2 {
     error Raffle_NotEnoughEthSent();
+    error Raffle_TransferFailed();
+    error Raffle_RaffleNotOpen();
+    error Raffle_UpkeepNotNeeded(
+        uint256 currentBalance,
+        uint256 numPlayers,
+        RaffleState raffleState
+    );
+
+    /** Type declarations  */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
 
     /** State Variables */
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
@@ -44,6 +57,7 @@ contract Raffle is VRFConsumerBaseV2 {
      * @param player The address of the player who entered the raffle.
      */
     event EnterRaffle(address indexed player);
+    event WinnerPicked(address indexed winner);
 
     uint256 private immutable i_entranceFee; // 参与价格
     // @dev Duration of the lottery in seconds
@@ -57,6 +71,7 @@ contract Raffle is VRFConsumerBaseV2 {
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
     address private s_recentWinner; // 近期获胜者
+    RaffleState private s_raffleState;
 
     constructor(
         uint256 _entranceFee,
@@ -74,6 +89,7 @@ contract Raffle is VRFConsumerBaseV2 {
         i_callbackGasLimit = _callbackGasLimit;
 
         s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
     }
 
     // 买彩票
@@ -81,6 +97,10 @@ contract Raffle is VRFConsumerBaseV2 {
         if (msg.value < i_entranceFee) {
             // 购买raffle的token 不足时报错回滚
             revert Raffle_NotEnoughEthSent(); // revert回滚消耗更少的gas
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            // 当购买彩票通道状态不是OPEN状态时，回滚
+            revert Raffle_RaffleNotOpen();
         }
 
         s_players.push(payable(msg.sender)); // 将买彩票的人的地址放入数组存储
@@ -90,15 +110,43 @@ contract Raffle is VRFConsumerBaseV2 {
         emit EnterRaffle(msg.sender);
     }
 
+    /**
+     * when is the winner supposed to be picked?
+     * @dev This is the function that the Chainlink Automation nodes call
+     * to see if it's time to perform an upkeep.(校验chainlink 的自动脚本是否执行)
+     * The following should be true in order to return true:
+     * 1. The time interval has passed between raffle runs.
+     * 2. The raffle is in the OPEN state.
+     * 3. The contract has ETH (which means players can enter).
+     * 4. (Additional) There is a player and an amount of ETH sent to the contract
+     *      that players can enter.
+     */
+    function checkUpkeep(
+        bytes memory /*checkData*/
+    ) public view returns (bool upkeepNeeded, bytes memory /*performData*/) {
+        bool timeHasPassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = (s_players.length > 0);
+        return (timeHasPassed && isOpen && hasBalance && hasPlayers, "0x0 ");
+    }
+
     // 开奖
     // 1. get a random number
     // 2. use the random number to pick a player.
     // 3. be automatically called 自动执行
-    function pickWinner() external {
+    function performUpkeep(bytes calldata /** performData */) external {
         // check to see if enough time has passed
-        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
-            revert();
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle_UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                RaffleState(s_raffleState)
+            );
         }
+
+        s_raffleState = RaffleState.CALCULATING;
         // 1. request the RNG 请求随机数
         // 2. get the radom number 获取随机数
         i_vrfCoordinator.requestRandomWords(
@@ -110,24 +158,39 @@ contract Raffle is VRFConsumerBaseV2 {
         );
     }
 
+    // CEI: Check Effects Interactions (开发方式，步骤)
     // 该函数调用chainlink 获取随机数， override重写这个函数
     function fulfillRandomWords(
-        uint256 requestId,
+        uint256 /* requestId */,
         uint256[] memory randomWords
     ) internal override {
+        // Checks
+        // Effects (Our own contract)
         // 除模参与人得到0-参与人长度的索引值，然后选中参与者数组里的人
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable winner = s_players[indexOfWinner];
         s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+
+        s_players = new address payable[](0); // 重置参与者
+        s_lastTimeStamp = block.timestamp;
+
+        // Interactions (Other contracts)
         // 将链上资金都给获胜者
         (bool success, ) = s_recentWinner.call{value: address(this).balance}(
             ""
         );
         if (!success) {
             // 失败的话回滚
-            revert Raffe__TransferFaild();
+            revert Raffle_TransferFailed();
         }
+
+        // 发送获胜者通知
+        emit WinnerPicked(s_recentWinner);
     }
 
     /** getter function */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
